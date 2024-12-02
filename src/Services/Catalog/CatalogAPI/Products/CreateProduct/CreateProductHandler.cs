ac751f479e193e1520dccc7fac555a97045eac5b
busrayalcinn <79150488+busrayalcinn@users.noreply.github.com>
2024-12-02 20:06:02 +0300

    Remove Manual Validator

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -18,7 +18,7 @@ public class CreateProductCommandValidator : AbstractValidator<CreateProductComm
 }
 
 internal class CreateProductCommandHandler
-    (IDocumentSession session, IValidator<CreateProductCommand> validator)
+    (IDocumentSession session, ILogger<CreateProductCommandHandler> logger)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -27,11 +27,8 @@ internal class CreateProductCommandHandler
         //save to database
         //return CreateProductResult result
 
-        var result = await validator.ValidateAsync(command, cancellationToken);
-        var errors= result.Errors.Select(x => x.ErrorMessage).ToList();
-        if (errors.Any()) {
-            throw new ValidationException(errors.FirstOrDefault());
-        }
+        logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
+
 
         var product = new Product
         {
