dab47a22899bbae5a2e6e3ae22df098f23d5e16f
busrayalcinn <79150488+busrayalcinn@users.noreply.github.com>
2024-12-03 20:21:35 +0300

    Add InternalServerException

--- src/BuildingBlocks/BuildingBlocks/Exceptions/InternalSErverException.cs
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/InternalSErverException.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BuildingBlocks.Exceptions
+﻿namespace BuildingBlocks.Exceptions;
+public class InternalServerException : Exception
 {
-    internal class InternalSErverException
+    public InternalServerException(string message) : base(message)
     {
     }
+
+    public InternalServerException(string message, string details) : base(message)
+    {
+        Details = details;
+    }
+
+    public string? Details { get; }
 }
