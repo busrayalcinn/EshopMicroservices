5a58e18fd48465ee57fa852f794af96913172bbb
busrayalcinn <79150488+busrayalcinn@users.noreply.github.com>
2024-12-11 17:03:03 +0300

    Register and Configure Marten DocumentDB library into Program.cs asp.net DI

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var assembly = typeof(Program).Assembly;
+
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
@@ -11,6 +12,12 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
