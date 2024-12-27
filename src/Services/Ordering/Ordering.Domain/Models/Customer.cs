fd86ba06dd9d850f6cddf298fe64fdf92800b6ad
busrayalcinn <79150488+busrayalcinn@users.noreply.github.com>
2024-12-27 21:30:09 +0300

    Develop Strongly typed IDs for Order Entities

--- src/Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -1,7 +1,5 @@
-﻿using Ordering.Domain.Abstractions;
-
-namespace Ordering.Domain.Models;
-public class Customer : Entity<Guid>
+﻿namespace Ordering.Domain.Models;
+public class Customer : Entity<CustomerId>
 {
     public string Name { get; private set; } = default!;
     public string Email { get; private set; } = default!;
