2f9ba7c8f36592f429c6ebce1995b34f09211470
busrayalcinn <79150488+busrayalcinn@users.noreply.github.com>
2025-01-09 22:52:12 +0300

    Health Checks for Ordering Microservices with EF.Core and SQL Server

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -6,11 +6,10 @@ using Ordering.Infrastructure.Data.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services
     .AddApplicationServices()
     .AddInfrastructureServices(builder.Configuration)
-    .AddApiServices();
+    .AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
