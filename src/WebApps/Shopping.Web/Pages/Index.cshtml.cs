16304104d84e1604f56bddbcaa6de69d01ffea9e
busrayalcinn <79150488+busrayalcinn@users.noreply.github.com>
2025-01-18 15:03:16 +0300

    AddToCart Submit Handle in Index cshtml Page with IBasketService

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -13,4 +13,26 @@ public class IndexModel
         ProductList = result.Products;
         return Page();
     }
+
+    public async Task<IActionResult> OnPostAddToCartAsync(Guid productId)
+    {
+        logger.LogInformation("Add to cart button clicked");
+
+        var productResponse = await catalogService.GetProduct(productId);
+
+        var basket = await basketService.LoadUserBasket();
+
+        basket.Items.Add(new ShoppingCartItemModel
+        {
+            ProductId = productId,
+            ProductName = productResponse.Product.Name,
+            Price = productResponse.Product.Price,
+            Quantity = 1,
+            Color = "Black"
+        });
+
+        await basketService.StoreBasket(new StoreBasketRequest(basket));
+
+        return RedirectToPage("Cart");
+    }
 }
